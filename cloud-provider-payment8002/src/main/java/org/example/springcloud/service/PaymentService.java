8d336e6330adfc8cc65e0d4760109935eaa2a388
DongLiusuo <535425450@qq.com>
2020-04-07 18:08:22 +0800

    initial

---
+++ cloud-provider-payment8002/src/main/java/org/example/springcloud/service/PaymentService.java
@@ -0,0 +1,9 @@
+package org.example.springcloud.service;
+
+import org.example.springcloud.entities.Payment;
+
+public interface PaymentService {
+    int create(Payment payment);
+    Payment getPaymentById(Long id);
+
+}
