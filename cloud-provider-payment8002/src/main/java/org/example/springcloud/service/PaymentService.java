1093dd25dbb55695d0c2509adb648be281303564
DongLiusuo <535425450@qq.com>
2020-04-07 18:12:33 +0800

    Revert "initial"
    This reverts commit 8d336e63

--- cloud-provider-payment8002/src/main/java/org/example/springcloud/service/PaymentService.java
+++
@@ -1,9 +0,0 @@
-package org.example.springcloud.service;
-
-import org.example.springcloud.entities.Payment;
-
-public interface PaymentService {
-    int create(Payment payment);
-    Payment getPaymentById(Long id);
-
-}
